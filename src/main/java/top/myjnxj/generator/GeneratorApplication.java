e59eecac013cfae0dbbd23f8a71253d466451162
江南小俊 <admin@example.com>
2019-01-19 13:58:44 +0800

    去除数据源配置

--- src/main/java/top/myjnxj/generator/GeneratorApplication.java
+++ src/main/java/top/myjnxj/generator/GeneratorApplication.java
@@ -1,13 +1,13 @@
 package top.myjnxj.generator;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class})
 @EnableSwagger2
-@MapperScan("top.myjnxj.generator.mapper")
 public class GeneratorApplication {
 
 	public static void main(String[] args) {
